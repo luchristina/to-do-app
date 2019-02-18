7b486dd0f4b89e6548d9e1dd92bbda8634c4e0bc
Christina <mailboxoftina@gmail.com>
2019-02-18 14:09:26 -0800

    dom script part 2 checkpoint

--- app.js
+++ app.js
@@ -1,65 +1,50 @@
 function onReady() {
+  const toDos = [];
   const addToDoForm = document.getElementById('addToDoForm');
-  const newToDoText = document.getElementById('newToDoText');
-  const toDoList = document.getElementById('toDoList');
 
-  addToDoForm.addEventListener('submit', () => {
+  function createNewToDo() {
+    const newToDoText = document.getElementById('newToDoText');
+    if (!newToDoText.value) { return; }
+
+    toDos.push({
+      title: newToDoText.value,
+      complete: false
+    });
+
+    newToDoText.value = '';
+
+    renderTheUI();
+  }
+
+  function renderTheUI() {
+    const toDoList = document.getElementById('toDoList');
+
+    toDoList.textContent = '';
+
+    toDos.forEach(function(toDo) {
+      const newLi = document.createElement('li');
+      const checkbox = document.createElement('input');
+
+      checkbox.type = "checkbox";
+
+      newLi.textContent = toDo.title;
+
+      toDoList.appendChild(newLi);
+      newLi.appendChild(checkbox);
+    });
+  }
+
+  addToDoForm.addEventListener('submit', event => {
     event.preventDefault();
+    createNewToDo();
+    renderTheUI();
+  });
 
-    // get the text
-    let title = newToDoText.value;
-
-    // create a new li
-    let newLi = document.createElement('li');
-
-    // create a new input
-    let checkbox = document.createElement('input');
-
-    // set the input's type to checkbox
-    checkbox.type = "checkbox";
-
-    // set the title
-    newLi.textContent = title;
-
-    // attach the checkbox to the li
-    newLi.appendChild(checkbox);
-
-    // attach the li to the ul
-    toDoList.appendChild(newLi);
-
-    // empty the input
-    newToDoText.value ='';
-
-    // create delete button
-    let deleteButton = document.createElement('input');
-
-    // create onclick for delete
-    let onClick = document.createAttribute("onclick");
-    onClick.value = "deleteButton(this)";
-
-    // create type for deleteButton
-    let type = document.createAttribute("type");
-    type.value = "button";
-
-    // create text value
-    let inputValue = document.createAttribute("value");
-    inputValue.value = "Delete"; set attributes
-
-    // delete set attributes
-    deleteButton.setAttributeNode(onClick);
-    deleteButton.setAttributeNode(type);
-    deleteButton.setAttributeNode(inputValue);
-    newLi.appendChild(deleteButton);
-
-});
+  renderTheUI();
 }
+
+
 
 window.onload = function() {
   onReady();
 };
-
-// delete button
-function deleteButton(btn) {
-  var row = btn.parentNode;
-  row.parentNode.removeChild(row);
-}
