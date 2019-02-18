d212c03e1b905a2742886c6f950826f97ec0dba9
Christina <mailboxoftina@gmail.com>
2019-02-18 15:05:24 -0800

    issue with removing item from filter

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 function onReady() {
   const toDos = [];
+  let id = 0;
   const addToDoForm = document.getElementById('addToDoForm');
 
   function createNewToDo() {
@@ -8,12 +9,15 @@ function onReady() {
 
     toDos.push({
       title: newToDoText.value,
-      complete: false
+      complete: false,
+      id: id.value
     });
 
     newToDoText.value = '';
 
     renderTheUI();
+
+    id++;
   }
 
   function renderTheUI() {
@@ -24,14 +28,28 @@ function onReady() {
     toDos.forEach(function(toDo) {
       const newLi = document.createElement('li');
       const checkbox = document.createElement('input');
+      const deleteButton = document.createElement('input');
+      let inputValue = document.createAttribute("value");
+      inputValue.value = "Delete";
 
       checkbox.type = "checkbox";
+      deleteButton.type = "button";
+      deleteButton.setAttributeNode(inputValue);
 
       newLi.textContent = toDo.title;
 
       toDoList.appendChild(newLi);
       newLi.appendChild(checkbox);
+      newLi.appendChild(deleteButton);
+
+      deleteButton.addEventListener('click', event => {
+        event.preventDefault();
+        newLi.parentNode.removeChild(newLi);
+        const removed = toDos.filter(id => toDos.id);
+          remove(toDos.id);
+      });
     });
+
   }
 
   addToDoForm.addEventListener('submit', event => {
